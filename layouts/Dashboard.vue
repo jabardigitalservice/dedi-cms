d9e8b248313da1db1845c3d160ee1145dacff60c
Adzhar Amrullah <adzharamrullah@gmail.com>
2021-12-29 13:36:13 +0700

    feat: dashboard user navigation

---
+++ layouts/Dashboard.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="dashboard">
+    <div class="dashboard__sidebar" />
+    <div class="dashboard__header">
+      <Header />
+      <div class="dashboard__content">
+        <Nuxt />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LayoutDashboard'
+}
+</script>
+
+<style lang="postcss">
+.dashboard {
+  background-image: url('~assets/images/background-login.svg');
+  @apply bg-no-repeat bg-bottom h-screen w-screen bg-[#EDEFF3]
+  w-full h-screen grid grid-cols-[minmax(260px,15%),1fr];
+
+  &__sidebar {
+    @apply bg-white;
+  }
+
+  &__header {
+    @apply w-full max-h-screen overflow-y-auto;
+  }
+
+  &__content {
+    @apply w-full h-[calc(100%-72px)] py-[14px] px-6;
+  }
+}
+</style>
