9cbd4e1e3648cab113fda6a97a73a7e1a0a8432c
Adzhar Amrullah <adzharamrullah@gmail.com>
2022-01-11 04:43:18 +0700

    feat: create component subheader

---
+++ components/SubHeader/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="subheader">
+    <h1 class="subheader__title">
+      {{ title }}
+    </h1>
+    <p v-if="true" class="subheader__descriptions">
+      {{ descriptions }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ComponentSubHeader',
+  props: {
+    title: {
+      type: String,
+      default: '',
+      required: true
+    },
+    descriptions: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="postcss">
+@import './SubHeader.pcss';
+</style>
