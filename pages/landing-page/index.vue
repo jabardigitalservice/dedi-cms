76fbeb26a53909f61b62c50f07fb34b30c6e9b91
Adzhar Amrullah <adzharamrullah@gmail.com>
2022-01-19 12:53:04 +0700

    feat: create landing page

--- pages/landing-page/index.vue
+++ pages/landing-page/index.vue
@@ -1,55 +1,19 @@
 <template>
-  <div class="flex flex-col gap-3">
+  <div class="landing-page">
     <SubMenu :menus="menus" @change="onChangeSubMenu" />
-    <SubHeader v-bind="header" />
-    <div class="py-[14px] px-6 rounded-lg bg-white">
-      <BaseDataTable
-        :headers="headerTable"
-        :items="dataHeroBanner"
-        :loading="$fetchState.pending"
-        :pagination="pagination"
-      />
-    </div>
+    <component :is="selectedSubMenu" />
   </div>
 </template>
 
 <script>
-import { headerTable, header, menus } from '@/constants/landingPage'
+import { menus } from '@/constants/landingPage'
 export default {
   name: 'PageLandingPage',
   layout: 'Dashboard',
   data () {
     return {
-      selectedSubMenu: null,
-      headerTable,
-      header,
       menus,
-      data: [],
-      pagination: {
-        currentPage: 1,
-        totalRows: 0,
-        itemsPerPage: 20,
-        itemsPerPageOptions: [20, 40, 60, 80, 100],
-        disabled: false
-      }
-    }
-  },
-  async fetch () {
-    const response = await this.$axios.get('/pages')
-    const { data, meta } = response.data
-    this.data = data || []
-    this.pagination.currentPage = meta?.current_page || 1
-    this.pagination.totalRows = meta?.total || 0
-    this.pagination.itemsPerPage = meta?.per_page || 20
-  },
-  computed: {
-    dataHeroBanner () {
-      return this.data.map((item) => {
-        return {
-          ...item,
-          status: item.is_active === true ? 'Aktif' : 'Non-Aktif'
-        }
-      })
+      selectedSubMenu: null
     }
   },
   mounted () {
@@ -63,6 +27,8 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="postcss">
+.landing-page {
+  @apply flex flex-col gap-[14px];
+}
 </style>
