3fedc04befe3dba487536ff6e6ac5c7500f5df04
Adzhar Amrullah <adzharamrullah@gmail.com>
2022-01-31 15:22:44 +0700

    feat: add page detail hero banner

---
+++ pages/landing-page/hero-banner/detail/_id/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <HeroBannerDetail :item="data" />
+</template>
+
+<script>
+export default {
+  name: 'PagesHeroBannerDetail',
+  layout: 'Dashboard',
+  async asyncData ({ params, $axios }) {
+    const response = await $axios.get(`/pages/${params.id}`)
+    const data = response.data?.data
+    return {
+      data
+    }
+  }
+}
+</script>
