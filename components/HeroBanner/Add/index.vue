55fbe0b55e806b464bbdf6189756581f4726869d
Adzhar Amrullah <adzharamrullah@gmail.com>
2022-01-31 12:24:35 +0700

    fix: regex validate url (#15)

--- components/HeroBanner/Add/index.vue
+++ components/HeroBanner/Add/index.vue
@@ -219,7 +219,7 @@ export default {
       this.dropFileErrorMessage = ''
     },
     onLinkInput (value) {
-      const expUrl = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi
+      const expUrl = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,})/gi
       const regexUrl = new RegExp(expUrl)
       if (!value.length || value.match(regexUrl)) {
         this.linkErrorMessage = ''
