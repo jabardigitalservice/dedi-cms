a83a31449632e2127d5d269bf3d82c049da32e8d
Adzhar Amrullah <adzharamrullah@gmail.com>
2022-02-10 15:27:50 +0700

    feat: add feat list view testimonials

---
+++ components/Testimonials/TableAction/index.vue
@@ -0,0 +1,81 @@
+<template>
+  <JdsPopover
+    class="datatable__action-button"
+    :value="isDropdownOpen"
+    :options="popoverOptions"
+  >
+    <template #activator>
+      <BaseButton
+        v-on-clickaway="closeDropdown"
+        variant="secondary"
+        size="small"
+        @click="toggleDropdown"
+      >
+        Aksi
+        <JdsIcon
+          name="chevron-down"
+          size="14px"
+          fill="#069550"
+          class="action__icon"
+          :class="isDropdownOpen ? '-rotate-180' : null"
+        />
+      </BaseButton>
+    </template>
+
+    <div class="action__dropdown">
+      <ul class="action__dropdown-list">
+        <li>
+          <button class="action__dropdown-item" @click="$emit('detail')">
+            Detail
+          </button>
+        </li>
+        <li>
+          <button class="action__dropdown-item" @click="$emit('edit')">
+            Ubah
+          </button>
+        </li>
+        <li>
+          <button class="action__dropdown-item" @click="$emit('delete')">
+            Hapus
+          </button>
+        </li>
+      </ul>
+    </div>
+  </JdsPopover>
+</template>
+
+<script>
+import { directive as onClickaway } from 'vue-clickaway'
+
+export default {
+  name: 'TestimonialsTableAction',
+  directives: {
+    onClickaway
+  },
+  data () {
+    return {
+      isDropdownOpen: false,
+      popoverOptions: {
+        modifiers: [
+          {
+            name: 'offset',
+            options: { offset: [0, 8] }
+          }
+        ]
+      }
+    }
+  },
+  methods: {
+    toggleDropdown () {
+      this.isDropdownOpen = !this.isDropdownOpen
+    },
+    closeDropdown () {
+      this.isDropdownOpen = false
+    }
+  }
+}
+</script>
+
+<style lang="postcss">
+@import './TestimonialsTableAction.pcss';
+</style>
--- components/Testimonials/index.vue
+++ components/Testimonials/index.vue
@@ -1,13 +1,158 @@
 <template>
-  <div class="min-h-[calc(100vh-200px)] " />
+  <!-- eslint-disable vue/valid-v-slot -->
+  <div class="testimonial">
+    <SubHeader v-bind="headerTestimonials" />
+    <div class="testimonial__datatable">
+      <div class="testimonial__datatable-header">
+        <div class="testimonial__datatable-header-box-right">
+          <jds-search
+            v-model="search"
+            small
+            icon
+            :button="false"
+            class="testimonial__datatable-header-search"
+            @input="onSearchChange"
+          />
+          <div class="testimonial__datatable-header-filter-icon">
+            <jds-icon name="filter-outline" size="16px" />
+          </div>
+          <span class="testimonial__datatable-header-filter-text">
+            Filter :
+          </span>
+          <button type="button" class="testimonial__datatable-header-filter-dropdown">
+            Belum ada filter
+          </button>
+        </div>
+        <BaseButton variant="secondary">
+          <template #icon>
+            <div class="testimonial__datatable-header-add">
+              <div class="testimonial__datatable-header-plus-icon">
+                <jds-icon size="14px" name="plus-bold" />
+              </div>
+              Tambah Testimonial
+            </div>
+          </template>
+        </BaseButton>
+      </div>
+      <BaseDataTable
+        :headers="headerTableTestimonials"
+        :items="listTestimonial"
+        :loading="$fetchState.pending"
+        :pagination="pagination"
+        @next-page="nextPage"
+        @previous-page="previousPage"
+        @page-change="pageChange"
+        @per-page-change="perPageChange"
+      >
+        <template #item.action="{item}">
+          <TestimonialsTableAction :item="item" />
+        </template>
+      </BaseDataTable>
+    </div>
+  </div>
 </template>
 
 <script>
+import { debounce } from 'lodash'
+import { headerTableTestimonials, headerTestimonials } from '@/constants/landingPage'
 export default {
-  name: 'ComponentTestimonials'
+  name: 'ComponentTestimonials',
+  data () {
+    return {
+      headerTableTestimonials,
+      headerTestimonials,
+      search: '',
+      testimonials: [],
+      pagination: {
+        currentPage: 1,
+        totalRows: 0,
+        itemsPerPage: 5,
+        itemsPerPageOptions: [],
+        disabled: false
+      },
+      query: {
+        q: null,
+        per_page: 5,
+        current_page: 1
+      }
+    }
+  },
+  async fetch () {
+    const responses = await this.$axios.get('/testimonials', { params: this.query })
+    const { data, meta } = responses.data || []
+    this.testimonials = data
+    this.pagination.currentPage = meta?.current_page || 1
+    this.pagination.totalRows = meta?.total || 0
+    this.pagination.itemsPerPage = meta?.per_page || this.query.per_page
+  },
+  computed: {
+    listTestimonial () {
+      return this.testimonials.map((item) => {
+        return {
+          name: item.name,
+          role: item.type === 'mitra' ? 'Mitra' : 'Masyarakat',
+          testimonial: item.description,
+          status: item.is_active ? 'Aktif' : 'Non-Aktif'
+        }
+      })
+    },
+    generateItemsPerPageOptions () {
+      const options = []
+      for (let i = 1; i <= 5; i++) {
+        options.push(this.pagination.itemsPerPage * i)
+      }
+      return options
+    }
+  },
+  watch: {
+    query: {
+      deep: true,
+      handler () {
+        this.$fetch()
+      }
+    }
+  },
+  mounted () {
+    this.pagination.itemsPerPageOptions = this.generateItemsPerPageOptions
+  },
+  methods: {
+    searchTitle: debounce(function (value) {
+      if (value.length > 2) {
+        this.query.q = value
+      } else {
+        this.query.q = null
+      }
+    }, 500),
+    onSearchChange (value) {
+      this.searchTitle(value)
+    },
+    refreshDatatable () {
+      this.query = {
+        q: null,
+        per_page: 5,
+        current_page: 1
+      }
+      this.$fetch()
+    },
+    perPageChange (value) {
+      if (value) {
+        this.query.per_page = value
+      }
+      this.query.current_page = 1
+    },
+    pageChange (value) {
+      this.query.current_page = value
+    },
+    nextPage (value) {
+      this.query.current_page = value
+    },
+    previousPage (value) {
+      this.query.current_page = value
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="postcss">
+@import './Testimonials.pcss';
 </style>
