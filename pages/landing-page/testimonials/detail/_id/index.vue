aad92b2b291afafd6966cf74df3f6241e1b0725f
Adzhar Amrullah <adzharamrullah@gmail.com>
2022-02-23 12:57:49 +0700

    feat: add feature view detail testimonials (#23)

---
+++ pages/landing-page/testimonials/detail/_id/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <TestimonialsDetail :item="data" @updated="updated" />
+</template>
+
+<script>
+export default {
+  name: 'PagesTestimonialsDetail',
+  layout: 'Dashboard',
+  async asyncData ({ params, $axios }) {
+    try {
+      const response = await $axios.get(`/testimonials/${params.id}`)
+      const data = response.data?.data
+      return {
+        data
+      }
+    } catch (error) {
+      error({ message: 'Gagal memuat data' })
+    }
+  },
+  methods: {
+    async updated () {
+      await this.$nuxt.refresh()
+    }
+  }
+}
+</script>
