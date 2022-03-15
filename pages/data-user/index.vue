263bb39ecd2f38bb2fb7d61cc615d6675506f172
Yoslie <yoslie.tan@gmail.com>
2022-03-15 09:58:52 +0700

    feat: add submenu navigation in master data user

--- pages/data-user/index.vue
+++ pages/data-user/index.vue
@@ -1,17 +1,34 @@
 <template>
-  <Maintenance />
+  <div class="data-user">
+    <SubMenu :menus="menus" @change="onChangeSubMenu" />
+    <Maintenance />
+  </div>
 </template>
 
 <script>
+import { menus } from '~/constants/dataUser'
 export default {
   name: 'PageDataUser',
   layout: 'Dashboard',
+  data () {
+    return {
+      menus,
+      selectedSubMenu: 'admin'
+    }
+  },
   mounted () {
     this.$store.commit('setPageName', 'Master Data - User')
+  },
+  methods: {
+    onChangeSubMenu (value) {
+      this.selectedSubMenu = value
+    }
   }
 }
 </script>
 
-<style>
-
+<style lang="postcss">
+.data-user {
+  @apply flex flex-col gap-[14px] min-h-[calc(100vh-72px-52px)];
+}
 </style>
