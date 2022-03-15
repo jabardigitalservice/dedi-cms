65dbd0c89fba4fdce528cc958187205ec3c90913
Yoslie <yoslie.tan@gmail.com>
2022-03-15 09:59:21 +0700

    feat: add constant js data user

---
+++ constants/dataUser.js
@@ -0,0 +1,18 @@
+export const menus = [
+  {
+    value: 'admin',
+    label: 'Admin'
+  },
+  {
+    value: 'mitra',
+    label: 'Mitra'
+  },
+  {
+    value: 'perangkat-desa',
+    label: 'Perangkat Desa'
+  },
+  {
+    value: 'masyarakat',
+    label: 'Masyarakat'
+  }
+]
