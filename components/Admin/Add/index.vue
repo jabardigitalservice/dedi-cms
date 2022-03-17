858bbb77d69b6fa09dc9c84375f92f57ee830bb7
Yoslie <yoslie.tan@gmail.com>
2022-03-17 11:04:17 +0700

    feat: create modal add user admin

---
+++ components/Admin/Add/index.vue
@@ -0,0 +1,280 @@
+<template>
+  <BaseModal
+    :show="modalShow"
+    :loading="loading"
+    :is-form-completed="isFormCompleted"
+    label-right-btn="Tambahkan"
+    title="Tambah - Administrator Baru"
+    @submit="onSubmit"
+    @close="onModalClose"
+  >
+    <form v-if="modalShow" class="form-add-admin">
+      <div class="grid grid-cols-4">
+        <div class="col-span-1">
+          <div
+            :class="{
+              'form-add-admin__image': true,
+              'form-add-admin__image--attached': isAttached
+            }"
+          >
+            <jds-icon size="14px" name="user" />
+          </div>
+        </div>
+        <div class="col-span-3">
+          <div class="form-add-admin__title">
+            Upload Foto Profile
+          </div>
+          <div>
+            <div>
+              Ukuran maksimal file adalah 1 Mb.
+            </div>
+            <div>
+              File yang didukung adalah .jpg dan .png
+            </div>
+          </div>
+          <div class="form-add-admin__button">
+            <button class="form-add-admin__button-btn" type="button" @click="$refs.file.click()">
+              Tambah File
+              <jds-icon class="ml-2" size="12px" name="plus-bold" />
+            </button>
+            <input
+              ref="file"
+              type="file"
+              hidden="true"
+              accept="image/png, image/jpeg, image/svg+xml"
+              @change="onFileChange"
+            >
+          </div>
+          <div v-if="fileImage">
+            Filename: {{ fileImage.get('file').name }}
+          </div>
+          <div v-else-if="uploadFileErrorMessage" class="text-red-700">
+            {{ uploadFileErrorMessage }}
+          </div>
+          <div v-else>
+            Belum ada file terpilih.
+          </div>
+        </div>
+      </div>
+      <div class="form-add-admin__form-group">
+        <jds-input-text
+          v-model="form.name"
+          name="nama"
+          label="Nama Administrator"
+          placeholder="Masukkan nama Administrator"
+        />
+      </div>
+      <div class="form-add-admin__form-group">
+        <jds-input-text
+          v-model="form.email"
+          name="email"
+          label="Email"
+          placeholder="Masukkan alamat email"
+        />
+      </div>
+      <div class="form-add-admin__form-group">
+        <BaseInputText
+          v-model="form.password"
+          name="password"
+          label="Kata Sandi"
+          placeholder="Masukkan kata sandi"
+          type="password"
+        />
+        <!-- <jds-input-text
+          v-model="form.password"
+          name="password"
+          label="Kata Sandi"
+          placeholder="Masukkan kata sandi"
+        /> -->
+      </div>
+    </form>
+  </BaseModal>
+</template>
+
+<script>
+export default {
+  name: 'ComponentAdminAdd',
+  props: {
+    /**
+     * make modal visible or not
+     */
+    show: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data () {
+    return {
+      modalShow: undefined,
+      isAttached: false,
+      loading: false,
+      form: {
+        name: '',
+        email: '',
+        password: '',
+        is_active: true,
+        avatar: '',
+        avatar_original_name: ''
+      },
+      fileImage: null,
+      uploadFileErrorMessage: ''
+    }
+  },
+  computed: {
+    optionsOrderBanner () {
+      const options = []
+      for (let i = 1; i <= 5; i++) {
+        options.push(i)
+      }
+      return options
+    },
+    isFormCompleted () {
+      return !!((
+        this.form.name.length &&
+        this.form.email.length &&
+        this.form.password.length &&
+        this.fileImage &&
+        !this.uploadFileErrorMessage.length
+      ))
+    }
+  },
+  watch: {
+    show: {
+      handler (val) {
+        this.modalShow = val
+      },
+      immediate: true
+    }
+  },
+  methods: {
+    onSubmit () {
+      this.loading = true
+      this.form.password_confirm = this.form.password
+      this.submitFile(this.fileImage)
+        .then((response) => {
+          this.form.avatar = response.source
+          this.form.avatar_original_name = response.original_name
+        })
+        .then(async () => {
+          try {
+            await this.$axios.post('/users', this.form)
+            this.$emit('close')
+            this.$store.dispatch('toast/showToast', { type: 'success', message: 'Data berhasil disimpan' })
+            this.$emit('added')
+            this.resetForm()
+          } catch (error) {
+            this.$store.dispatch('toast/showToast', { type: 'error', message: 'Data gagal disimpan, periksa kembali data yang dinputkan' })
+          }
+        })
+        .catch(() => {
+          this.$store.dispatch('toast/showToast', { type: 'error', message: 'Gambar hero banner gagal diupload' })
+        }).finally(() => {
+          this.loading = false
+        })
+    },
+    onModalClose () {
+      this.$emit('close')
+      this.resetForm()
+    },
+    resetForm () {
+      this.form = {
+        name: '',
+        email: '',
+        password: '',
+        password_confirm: '',
+        is_active: 'true',
+        avatar: '',
+        avatar_original_name: ''
+      }
+      this.fileImage = null
+      this.isAttached = false
+      this.uploadFileErrorMessage = ''
+    },
+    submitFile (image) {
+      return new Promise((resolve, reject) => {
+        this.$axios.post('/files/upload', image, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        }).then((response) => {
+          const { data } = response.data
+          resolve(data)
+        }).catch((error) => {
+          reject(error)
+        })
+      })
+    },
+    setFile (value) {
+      const formData = new FormData()
+      formData.append('file', value)
+      this.fileImage = formData
+    },
+    onFileChange () {
+      if (this.$refs.file.files[0]) {
+        const isValidFormat = ['image/png', 'image/jpeg'].includes(this.$refs.file.files[0].type)
+        if (isValidFormat) {
+          if (this.$refs.file.files[0].size > 1000000) {
+            this.fileImage = null
+            this.uploadFileErrorMessage = 'Gambar anda melebihi ukuran maksimal'
+            this.isAttached = false
+          } else {
+            this.uploadFileErrorMessage = ''
+            this.isAttached = true
+            this.setFile(this.$refs.file.files[0])
+          }
+        } else {
+          this.fileImage = null
+          this.uploadFileErrorMessage = 'Maaf file yang anda masukan tidak didukung'
+          this.isAttached = false
+        }
+      } else {
+        this.fileImage = null
+        this.isAttached = false
+      }
+    }
+  }
+}
+</script>
+
+<style lang="postcss">
+.form-add-admin {
+  @apply text-sm leading-[23px] text-gray-800 py-4;
+
+  &__image {
+    @apply w-[88px] h-[88px] bg-gray-50 text-gray-400 flex justify-center items-center
+    border-2 border-gray-400 rounded-full box-border border-dashed stroke-dash-2;
+
+    &--attached {
+      @apply border-green-700 bg-green-50;
+    }
+  }
+
+  &__title {
+    @apply font-roboto text-gray-900 text-base leading-[23px] font-medium mb-1;
+  }
+
+  &__button {
+    @apply my-3 relative inline-block;
+
+    &-btn {
+      @apply bg-green-700 text-white w-[135px] h-[38px]
+      text-sm py-[9px] font-sans font-bold rounded-lg;
+
+      &:hover {
+        @apply bg-green-800;
+      }
+    }
+
+    input[type=file] {
+      position: absolute;
+      left: 0;
+      top: 0;
+      opacity: 0;
+    }
+  }
+
+  &__form-group {
+    @apply mt-6 w-full;
+  }
+}
+</style>
