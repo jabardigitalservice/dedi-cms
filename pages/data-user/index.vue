6a1ba1690cb52087dc800a1a4347360139a47d4e
Adzhar Amrullah <adzharamrullah@gmail.com>
2022-03-18 15:30:13 +0700

    fix: navigation route menu and sub menu (#28)

--- pages/data-user/index.vue
+++ pages/data-user/index.vue
@@ -16,7 +16,7 @@ export default {
   data () {
     return {
       menus,
-      selectedSubMenu: 'Admin',
+      selectedSubMenu: null,
       isMaintenance: false
     }
   },
--- store/index.js
+++ store/index.js
@@ -1,14 +1,24 @@
 export const state = () => ({
   pageName: 'Dashboard',
-  subMenu: null
+  subMenu: null,
+  isSameRoutePage: false
 })
 
 export const mutations = {
   setPageName (state, pageName) {
+    if (state.pageName === pageName) {
+      this.commit('setTrackPage', true)
+    } else {
+      this.commit('setTrackPage', false)
+    }
     state.pageName = pageName
   },
 
   setSubMenu (state, subMenu) {
     state.subMenu = subMenu
+  },
+
+  setTrackPage (state, value) {
+    state.isSameRoutePage = value
   }
 }
