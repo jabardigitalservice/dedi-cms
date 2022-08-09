ede403cdd6144d8c1a1796d2c4a67473dcd8de65
Yoslie <yoslie.tan@gmail.com>
2022-08-09 15:12:03 +0700

    feat: add list data table village in page master data village

---
+++ components/Village/TableAction/index.vue
@@ -0,0 +1,87 @@
+<template>
+  <JdsPopover
+    class="datatable__action-button"
+    :value="isDropdownOpen"
+    :options="popoverOptions"
+  >
+    <template #activator>
+      <BaseButton
+        v-on-clickaway="closeDropdown"
+        variant="secondary"
+        size="small"
+        @click="toggleDropdown"
+      >
+        Aksi
+        <JdsIcon
+          name="chevron-down"
+          size="14px"
+          fill="#069550"
+          class="action__icon"
+          :class="isDropdownOpen ? '-rotate-180' : null"
+        />
+      </BaseButton>
+    </template>
+
+    <div class="action__dropdown">
+      <ul class="action__dropdown-list">
+        <li>
+          <button class="action__dropdown-item" @click="$emit('detail')">
+            Detail
+          </button>
+        </li>
+        <li>
+          <button class="action__dropdown-item" @click="$emit('edit')">
+            Ubah
+          </button>
+        </li>
+        <li>
+          <button class="action__dropdown-item" @click="$emit('delete')">
+            Hapus
+          </button>
+        </li>
+      </ul>
+    </div>
+  </JdsPopover>
+</template>
+
+<script>
+import { directive as onClickaway } from 'vue-clickaway'
+
+export default {
+  name: 'VillageTableAction',
+  directives: {
+    onClickaway
+  },
+  props: {
+    isActive: {
+      type: Boolean,
+      default: true
+    }
+  },
+  data () {
+    return {
+      isDropdownOpen: false,
+      popoverOptions: {
+        modifiers: [
+          {
+            name: 'offset',
+            options: { offset: [0, 8] }
+          }
+        ]
+      }
+    }
+  },
+  methods: {
+    toggleDropdown () {
+      this.isDropdownOpen = !this.isDropdownOpen
+    },
+    closeDropdown () {
+      this.isDropdownOpen = false
+    }
+  }
+}
+</script>
+
+<style lang="postcss">
+@import '../Village.pcss';
+</style>
