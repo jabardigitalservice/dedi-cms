d06b100926e5c31cf7819a1f7b3b229a822ab7ee
Doohan <doohanaditya@yahoo.co.id>
2022-08-25 09:34:15 +0700

    chore (data-village): add option level variable

--- constants/dataVillage.js
+++ constants/dataVillage.js
@@ -24,3 +24,26 @@ export const headerTableDataVillage = [
     text: 'Aksi'
   }
 ]
+
+export const optionsLevel = [
+  {
+    value: null,
+    label: 'Belum ada level'
+  },
+  {
+    value: 1,
+    label: 1
+  },
+  {
+    value: 2,
+    label: 2
+  },
+  {
+    value: 3,
+    label: 3
+  },
+  {
+    value: 4,
+    label: 4
+  }
+]
