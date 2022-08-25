f0193ec251c90c21a665108d1a272881d876e1b7
Doohan <doohanaditya@yahoo.co.id>
2022-08-25 10:35:08 +0700

    refactor (edit-village): replace the regex variable from improting variable

--- components/Village/Edit/index.vue
+++ components/Village/Edit/index.vue
@@ -115,7 +115,7 @@
 
 <script>
 import { isEqual, cloneDeep } from 'lodash'
-import { optionsLevel } from '@/constants/dataVillage'
+import { optionsLevel, formatText, formatId, regexPoint } from '@/constants/dataVillage'
 export default {
   name: 'ComponentVillageEdit',
   props: {
@@ -235,13 +235,9 @@ export default {
       deep: true
     },
     'form.id' () {
-      // This pattern for checking user only allow input number and character (.)
-      const formatText = /(?=.*[^\d.])/g
       if (formatText.test(this.form.id)) {
         this.form.id = ''
       }
-      // formatId is pattern for id village maximal 13 chars (10 digit and 3 symbol) => XX.XX.XX.XXXX
-      const formatId = /(\B(?=(\d{2})+(?!\d))(?=.{4}))/g
       const newId = this.form.id.replaceAll(/[^0-9]/g, '').replace(formatId, '.')
       this.form.id = newId
       if (!this.form.id.length || this.form.id.length === 13) {
@@ -267,7 +263,6 @@ export default {
       }
     },
     'form.longitude' () {
-      const regexPoint = /^-?(\d+)(\.\d+)$/
       if (regexPoint.test(this.form.longitude) || !this.form.longitude.length) {
         this.errors.longitude = null
       } else {
@@ -275,7 +270,6 @@ export default {
       }
     },
     'form.latitude' () {
-      const regexPoint = /^-?(\d+)(\.\d+)$/
       if (regexPoint.test(this.form.latitude) || !this.form.latitude.length) {
         this.errors.latitude = null
       } else {
