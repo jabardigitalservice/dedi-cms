a02452e456386734ae2dcf7139a1d1002582fee2
Doohan <doohanaditya@yahoo.co.id>
2022-08-25 09:35:50 +0700

    refactor (add-village): replace the options level village from importing variable

--- components/Village/Add/index.vue
+++ components/Village/Add/index.vue
@@ -113,6 +113,7 @@
 </template>
 
 <script>
+import { optionsLevel } from '@/constants/dataVillage'
 export default {
   name: 'ComponentVillageAdd',
   props: {
@@ -148,28 +149,7 @@ export default {
       },
       listCity: [],
       listDistrict: [],
-      optionsLevel: [
-        {
-          value: null,
-          label: 'Belum ada level'
-        },
-        {
-          value: 1,
-          label: 1
-        },
-        {
-          value: 2,
-          label: 2
-        },
-        {
-          value: 3,
-          label: 3
-        },
-        {
-          value: 4,
-          label: 4
-        }
-      ],
+      optionsLevel,
       isDisabledOptionDistricts: true
     }
   },
